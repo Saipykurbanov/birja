5583a1b5fc1bbf224d26b1d2b967553f4833a15e
Saipykurbanov <kurbanoviliyas@mail.ru>
2024-12-04 23:19:53 +0300

    upd

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import './css/panel.css';
+import { NavLink } from 'react-router-dom';
 
 
 const Panel = () => {
     return (
-        <div>
-            
+        <div className='panel'>
+            <img src="/images/logo2.jpg" alt="" />
+
+            <h3>Dashboard</h3>
+            <div className="nav">
+                <div className="nav_item active">
+                    <h4>Analytic</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Auction management</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Shop management</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Inventory</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Customers</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Bid management</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Orders</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Finance</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Marketing</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Vendors</h4>
+                </div>
+                <div className="nav_item">
+                    <h4>Setting</h4>
+                </div>
+
+            </div>
         </div>
     );
 };
