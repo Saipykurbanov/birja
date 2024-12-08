a12baff06ee5cd885b5c7307f2fee6bab8e1b7b0
salimKurbanov <salim.kurbanov08@gmail.com>
2024-12-08 13:54:01 +0300

    upd

--- src/pages/stock/components/Table.jsx
+++ src/pages/stock/components/Table.jsx
@@ -6,8 +6,26 @@ const Table = () => {
 
     const [rows, setRows] = useState([
         { id: 1, col1: '123456', qr: '/icons/qr.svg' },
-        { id: 2, col1: '123456', qr: 'is Awesome' },
-        { id: 3, col1: '123456', qr: 'is Amazing' },
+        { id: 2, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 3, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 4, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 5, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 6, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 7, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 8, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 9, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 10, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 11, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 12, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 13, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 14, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 15, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 16, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 17, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 18, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 19, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 20, col1: '123456', qr: '/icons/qr.svg' },
+        { id: 21, col1: '123456', qr: '/icons/qr.svg' },
       ]);
       
       const columns = [
@@ -37,7 +55,7 @@ const Table = () => {
         <div className='table_wrapper'>
             <DataGrid 
                 rows={rows} 
-                columns={columns} 
+                columns={columns}
                 sx={{
                     '& .MuiDataGrid-columnHeader': {
                         backgroundColor: 'rgba(60, 60, 60, 1)',
@@ -46,6 +64,7 @@ const Table = () => {
                         textTransform: 'uppercase',
                         color: 'var(--white)',
                         height: '44px !important',
+                        border: '0',
                     },
                     '& .MuiDataGrid-columnHeader svg path': {
                         fill: 'var(--white)',
@@ -54,10 +73,11 @@ const Table = () => {
                         fontSize: '16px !important',
                         height: '44px !important',
                         display: 'flex',
-                        alignItems: 'center'
+                        alignItems: 'center',
+                        border: '1px solid var(--black)',
                     },
                     '& .MuiDataGrid-row': {
-                        minHeight: '44px !important'
+                        minHeight: '44px !important',
                     },
                     '& .MuiDataGrid-cell.first': {
                         backgroundColor: 'rgba(223, 223, 223, 1)'
