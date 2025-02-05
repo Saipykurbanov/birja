cbbde969c8af54d7cd179b21ec9792ca672848a6
Saipykurbanov <kurbanoviliyas@mail.ru>
2025-02-05 22:03:20 +0300

    upd

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,8 +3,8 @@ import Notice from "../components/notice/Notice"
 const Api = {}
 
 Api.mainUrl = 'http://185.127.225.49:'
-// Api.url = 'http://185.127.225.49:8083/'
-Api.url = 'http://188.120.229.3:8083/'
+Api.url = 'http://185.127.225.49:8083/'
+// Api.url = 'http://188.120.229.3:8083/'
 Api.url2 = 'http://185.127.225.49:8084/'
 Api.port3 = '8085/'
 
